819acc41effbb3abcf52b1d27a279502d71019a7
damonfo <damonfoo.au@gmail.com>
2022-10-07 20:26:17 +1000

    version 1.0

--- main.py
+++ main.py
@@ -9,8 +9,33 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.title("Turtle Cross the Road")
 
+car_manager = CarManager()
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.move_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+# Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.update_point()
+
+    # if player.distance(car)
+
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.write_level()
+
+    def write_level(self):
+        self.goto(-220, 250)
+        self.write(f"Level:{self.level}", align="center", font=FONT)
+
+    def update_point(self):
+        self.level += 1
+        self.clear()
+        self.write_level()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
